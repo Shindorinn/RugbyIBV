2ae3d368e060bf63d42fdb9dd93e6eb030d18f97
Dustin Meijer <du.a.meijer@googlemail.com>
2015-10-16 21:17:04 +0200

    Added a bit of a code and a second kernel

--- Filters/BasicKernel.cs
+++ Filters/BasicKernel.cs
@@ -8,16 +8,47 @@ namespace INFOIBV.Filters
 {
     public abstract class BasicKernel
     {
-        private int width;
-        private int height;
+        protected int width;
+        protected int height;
+        protected float[,] weights; // Need a new name for this.
 
         public BasicKernel(int width, int height)
         {
+            // We should really check if the height and width are of odd value and above zero here.
+            
             this.width = width;
             this.height = height;
+            this.weights = this.initializeWeights();
         }
 
         public abstract void apply(Bitmap imageToProcess);
+
+        private float[,] initializeWeights()
+        {
+            return this.initializeWeights(this.width, this.height);
+        }
+
+        private float[,] initializeWeights(int width, int height) 
+        {
+            // This is an example do nothing kernel, all zeroes except the middle.
+
+            // First initialize everything to 0,0
+            // [y, x] apparently. see -> https://msdn.microsoft.com/en-us/library/2yd9wwz4.aspx
+            float[,] toReturn = new float[height, width];
+            for (int x = 0; x < width; x++){
+                for (int y = 0; y < height; y++){
+                    toReturn[y, x] = 0;
+                }
+            }
+
+            // Then determine middle coordinate
+            int midY = (int) Math.Floor((float)height/2);
+            int midX = (int) Math.Floor((float)width/2);
+
+            toReturn[midY, midX] = 1;
+
+            return toReturn;
+        }
 
     }
 }
