20ac9e9830f9eb47950d349a85e1efd17edf6679
Dustin Meijer <du.a.meijer@googlemail.com>
2015-10-16 19:51:34 +0200

    Initial start of the Filters

---
+++ Filters/BasicKernel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace INFOIBV.Filters
+{
+    public abstract class BasicKernel
+    {
+        private int width;
+        private int height;
+
+        public BasicKernel(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public abstract void apply(Bitmap imageToProcess);
+
+    }
+}
