7e088bf4b4ae2cc5158763df688b7cc17ac9b425
Gliath <lukebuitendijk@gmail.com>
2015-10-22 12:34:37 +0200

    Added Basic Factory, not functional at the moment

---
+++ FilterFactory.cs
@@ -0,0 +1,20 @@
+﻿using INFOIBV.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INFOIBV
+{
+    public static class FilterFactory
+    {
+        public static BasicKernel Construct(List<FilterType> filters)
+        {
+
+
+
+            return null;
+        }
+
+    }
+}
