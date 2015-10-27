7f87318e6c24eb0b9b51f92a6e26bdb095ac44fc
Gliath <lukebuitendijk@gmail.com>
2015-10-27 15:02:07 +0100

    Added WIP kernels + Direction enum

---
+++ Filters/Direction.cs
@@ -0,0 +1,14 @@
+﻿namespace INFOIBV.Filters
+{
+    public enum Direction
+    {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest // Where is Kanye West?
+    }
+}
